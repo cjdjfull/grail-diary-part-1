6e6f50cb04e5ef504ee4b09d07c5699d5d947f9e
cedric janssens <cedric-janssens@lambdastudent.com>
2020-04-14 19:02:15 -0400

    not done yet :(

---
+++ Grail Diary/Grail Diary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grail Diary
+//
+//  Created by Cedric Janssens on 4/14/20.
+//  Copyright © 2020 Cedric Janssens. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
